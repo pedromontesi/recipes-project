1a9cefc0b4842c97d89092e59f597ae01a9b69c7
pedromontesi <joaopedromonteiro090@hotmail.com>
2025-11-11 14:22:43 -0300

    better recipe api

--- QrcodeFacade.php
+++ QrcodeFacade.php
@@ -1,6 +1,26 @@
 <?php
 
-class QrcodeFacade {
+use chillerlan\QRCode\{QRCode};
+
+class QrcodeFacade
+{
+
+    private string $data;
+
+
+    public function __construct(string $data)
+    {
+        $this->data = $data;
+
+    }
+
+
+    public function getQrcode()
+    {
+        $qrcode = new QRCode();
+        return $qrcode->render($this->data);
+    }
+
 
 
 }
--- form.php
+++ form.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once "RecipeForm.php";
-require_once "RecipeApi.php"; // importante incluir
+require_once "RecipeApi.php";
 
 ?>
 <!doctype html>
@@ -23,31 +23,5 @@ require_once "RecipeApi.php"; // importante incluir
         <button type="submit">Buscar</button>
     </form>
 </div>
-
-<?php
-if (!empty($_GET['ingredients'])) {
-    try {
-        $ingredient = trim($_GET['ingredients']);
-        $recipeApi = new RecipeApi($ingredient);
-        $meals = $recipeApi->getMeals();
-
-        if (!empty($meals)) {
-            echo "<h2>Receitas encontradas para: " . htmlspecialchars($ingredient) . "</h2>";
-            echo "<ul>";
-            foreach ($meals as $meal) {
-                echo "<li>";
-                echo "<strong>" . htmlspecialchars($meal['strMeal']) . "</strong><br>";
-                echo "<img src='" . htmlspecialchars($meal['strMealThumb']) . "' width='120'><br>";
-                echo "</li>";
-            }
-            echo "</ul>";
-        } else {
-            echo "<p>Nenhuma receita encontrada para '" . htmlspecialchars($ingredient) . "'.</p>";
-        }
-    } catch (Exception $e) {
-        echo "<p style='color:red;'>Erro: " . htmlspecialchars($e->getMessage()) . "</p>";
-    }
-}
-?>
 </body>
 </html>
